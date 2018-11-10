078d2c49c8648bebfdf227335cc25fb6e6c163f5
fpapado <fotis@futurice.com>
2018-11-10 12:08:13 +0200

    V1 of critical css inlining

---
+++ postprocessing/critical-css-plugin.js
@@ -0,0 +1,162 @@
+const path = require("path");
+const fs = require("fs");
+const multimatch = require("multimatch");
+const invariant = require("invariant");
+const Purgecss = require("purgecss");
+const purgeHtml = require("purgecss-from-html");
+const gzipSize = require("gzip-size");
+const cheerio = require("cheerio");
+
+// Set up debug
+const debug = require("debug")("critical-css-plugin");
+
+/**
+ * Expose `plugin`.
+ */
+module.exports = plugin;
+
+/**
+ * Metalsmith plugin to inline critical css, and load the rest asynchronously.
+ *
+ * @arg cssPublicPath:
+ *  The path under which the css is included in the template.
+ *  Important for knowing which <link> tag to replace.
+ *
+ * @return {Function}
+ */
+
+function plugin({ pattern, cssFile, cssPublicPath } = {}) {
+  invariant(!!pattern, "You must supply a pattern for the html files.");
+  invariant(
+    !!cssFile && !Array.isArray(cssFile),
+    "You must supply a single css file to look for. Multiple files are not currently supported"
+  );
+
+  return function(files, metalsmith, done) {
+    debug("Will read CSS file", cssFile);
+    const cssFileHandle = path.resolve(cssFile);
+    const cssContent = fs.readFileSync(cssFileHandle, { encoding: "utf-8" });
+    debug("Read CSS file OK");
+
+    // Loop over all the files, applying the transform if matching
+    Object.keys(files)
+      .filter(file => multimatch(file, pattern).length > 0)
+      .forEach(function(file) {
+        debug(`${file} matches pattern`);
+
+        // utf-8 decode read file contents
+        debug("Will read file");
+        let fileContent = files[file].contents;
+        if (!!fileContent && fileContent instanceof Buffer) {
+          fileContent = fileContent.toString("utf-8");
+        }
+        debug("Read file OK");
+
+        debug("Will get used CSS");
+        const usedCss = getUsedCss({
+          htmlContent: fileContent,
+          cssContent: cssContent
+        });
+        debug("Got used CSS OK");
+
+        // NOTE: The gzip size will be even smaller when inlined into the document,
+        // because the classes are shared
+        debug(`Used CSS gzip-size (standalone): ${gzipSize.sync(usedCss)} B`);
+
+        debug("Will inject used CSS to file contents");
+        const htmlWithInline = inlineCriticalCss({
+          htmlContent: fileContent,
+          cssPublicPath: cssPublicPath,
+          criticalCssContent: usedCss
+        });
+        debug("Critical CSS injection OK");
+
+        debug("Will write to file contents");
+        files[file].contents = htmlWithInline;
+        debug("Wrote file contents");
+
+        debug("Critical CSS plugin OK");
+        done();
+      });
+  };
+}
+
+//
+// HELPERS
+
+//
+// Return only the css from cssContent that is used in htmlContent
+// Might have false negatives where JS interaction is concerned, but
+// those should be minimal and in any case the full css should come
+// in before that.
+const getUsedCss = ({ htmlContent, cssContent }) => {
+  const purgeCss = new Purgecss({
+    content: [
+      {
+        raw: htmlContent,
+        extension: "html"
+      }
+    ],
+    css: [
+      {
+        raw: cssContent,
+        extension: "css"
+      }
+    ],
+    extractors: [
+      {
+        extractor: purgeHtml,
+        extensions: ["html"]
+      }
+    ]
+  });
+
+  // The result of purgeCss.purge() is an array because of multiple files.
+  // We only have one file, so we take the first one.
+  const usedCss = purgeCss.purge()[0].css;
+  return usedCss;
+};
+
+//
+// Inline Critical CSS in HTML, by replacing <link rel="stylesheet">
+// with a preload tag, adding the critical CSS as <style>, and using
+// loadCSS as a polyfill
+function inlineCriticalCss({ htmlContent, cssPublicPath, criticalCssContent }) {
+  // Set up new markup
+  const style = `<style>${criticalCssContent}</style>`;
+
+  // Manipulate markup
+  const $ = cheerio.load(htmlContent);
+
+  // Find the relevant link tag
+  const $link = $('link[rel="stylesheet"]').filter(`[href="${cssPublicPath}"]`);
+
+  // If no relevant link tags found, return the original content
+  if ($link.length === 0) {
+    debug("Found no link tags with cssPublicPath of " + cssPublicPath);
+    return htmlContent;
+  }
+
+  // NOTE: .html() returns '' for some reason, so we use toString() instead...
+  const linkStylesheet = $link.toString();
+
+  const noscriptFallback = `<noscript>${linkStylesheet}</noscript>`;
+
+  // link rel="stylesheet" -> <link rel="preload" href="path/to/mystylesheet.css" as="style" onload="this.rel='stylesheet'">
+  $link
+    .attr({
+      rel: "preload",
+      as: "style",
+      // eslint-disable-next-line quotes
+      onload: `this.rel='stylesheet'`
+    })
+    .append(noscriptFallback)
+    .append(style);
+
+  // TODO: Inline LoadCSS;
+  // Inline LoadCSS as a polyfill
+  // @see https://github.com/filamentgroup/loadCSS
+
+  const newHtml = $.html();
+  return newHtml;
+}
---
+++ postprocessing/process.js
@@ -0,0 +1,53 @@
+/*
+ * This file is responsible for postprocessing the html + assets output after Eleventy. Metalsmith is used because it can carry files through a pipeline, and it normalises the directory/file search.
+ *
+ * Here's what we do at the moment:
+ *  - Read all the files from _site
+ *  - Postprocess *.html and the linked index.css such that the css selectors and rules used are inlined. This helps the first page load significantly. Then, ake the css script async via link rel="preload" and use LoadCSS as a fallback.
+ *
+ * In the future, we might:
+ *  - Hash and rewrite references to css and js files, for long-term caching
+*/
+
+const metalsmith = require("metalsmith");
+const criticalCss = require("./critical-css-plugin");
+
+// TODO: Could take this from CLI args
+const INPUT_DIR = "_site/";
+const OUTPUT_DIR = "_site/";
+
+function main() {
+  metalsmith(process.cwd())
+    .source(INPUT_DIR) // source directory
+    .destination(OUTPUT_DIR) // destination directory
+    .clean(false) // clean destination before
+    .use(
+      criticalCss({
+        pattern: "**/*.html",
+        // The CSS file whose selectors will be matched against the html
+        cssFile: "./_site/css/index.css",
+        // The path under which the css is included in the template
+        cssPublicPath: "/css/index.css"
+      })
+    )
+    .build(function(err) {
+      if (err) {
+        console.log("Error running the postprocessing pipeline: " + err);
+        throw err;
+      }
+      console.log("Done!");
+    });
+}
+
+main();
+
+//
+// UTIL
+
+/** Metalsmith plugin wrapper that runs the plugin only in production */
+function productionOnly(plugin) {
+  return (files, metalsmith, done) =>
+    process.env.NODE_ENV === "production"
+      ? plugin(files, metalsmith, done)
+      : done();
+}
