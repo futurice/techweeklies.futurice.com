66ac2e14dbab781715e796515fe4a4d770b40de3
fpapado <fotis@futurice.com>
2018-11-14 23:45:29 +0200

    Add a basic styleguide, fix Text lineheight

--- src/_includes/components/Text.js
+++ src/_includes/components/Text.js
@@ -1,6 +1,6 @@
 module.exports = function(content, { className = '' } = {}) {
   return `
-    <p class="mv0 f4 lh-title ${className}">
+    <p class="mv0 f4 lh-copy ${className}">
         ${content}
     </p>`;
 };
