591cfb6f714359b6e129f43f64f7a40be27353a4
fpapado <fotis@futurice.com>
2018-11-15 11:45:46 +0200

    Fill in hover, button states

--- src/_includes/components/Link.js
+++ src/_includes/components/Link.js
@@ -7,7 +7,7 @@ module.exports = function(
     : '';
 
   return `
-    <a href="${href}" class="fg-accent focus-outline-accent focus-tdn ${className}" ${externalAttrs}>
+    <a href="${href}" class="fg-accent focus-outline-accent hover-fg-accent-2 transition-fg focus-tdn ${className}" ${externalAttrs}>
         ${content}
     </a>`;
 };
